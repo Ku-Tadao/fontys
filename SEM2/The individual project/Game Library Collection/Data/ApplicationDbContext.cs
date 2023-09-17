241213974e43490ed1031c9801b0b2acd24ad35b
Ku Tadao <kubilay12344@hotmail.nl>
2023-09-17 18:47:14 +0200

    INIT Individual Project

---
+++ SEM2/The individual project/Game Library Collection/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Game_Library_Collection.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
